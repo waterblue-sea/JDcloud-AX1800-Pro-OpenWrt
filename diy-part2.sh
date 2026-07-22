0445f4c3490781bcb1a448612dfa12875de64b94
Zirui Shao <purewater@mail.ustc.edu.cn>
2026-07-23 02:13:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,7 +2,7 @@
 
 logger -t "DIY-Builder" "Libwrt-pure"
 
-sed -i 's/192.168.1.1/172.18.16.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.18.0.1/g' package/base-files/files/bin/config_generate
 
 sed -i '/smartdns/d' .config 2>/dev/null || true
 sed -i '/luci-app-smartdns/d' .config 2>/dev/null || true
@@ -43,7 +43,7 @@ uci -q set firewall.@defaults[0].flow_offloading='0'
 uci -q set firewall.@defaults[0].flow_offloading_hw='0'
 uci commit firewall
 
-uci set network.lan.ipaddr='172.18.16.1'
+uci set network.lan.ipaddr='10.18.0.1'
 uci set network.lan.netmask='255.255.255.0'
 uci set dhcp.lan.start='100'
 uci set dhcp.lan.limit='150'
@@ -68,83 +68,83 @@ uci -q set wireless.@wifi-iface[1].key='12345678' || uci -q set wireless.default
 uci commit wireless
 
 if [ ! -f "/etc/config/storage_matrix_done" ]; then
-    logger -t "Storage-Init" "Mermory"
-    
-    mkfs.ext4 -F /dev/mmcblk0p26 2>/dev/null
-    mkfs.ext4 -F /dev/mmcblk0p25 2>/dev/null
-    PART_2G=$(ls -1 /dev/mmcblk0p* 2>/dev/null | grep -E "p27|p28" | head -n 1)
-    [ -n "$PART_2G" ] && mkfs.ext4 -F $PART_2G 2>/dev/null
-    
-    UUID_P26=$(block info /dev/mmcblk0p26 2>/dev/null | awk -F"UUID=" '{print $2}' | cut -d'"' -f2)
-    UUID_P25=$(block info /dev/mmcblk0p25 2>/dev/null | awk -F"UUID=" '{print $2}' | cut -d'"' -f2)
-    UUID_P2G=$(block info $PART_2G 2>/dev/null | awk -F"UUID=" '{print $2}' | cut -d'"' -f2)
-    
-    if [ -n "$UUID_P26" ] && [ -n "$UUID_P25" ]; then
-        cat << FSTAB > /etc/config/fstab
+logger -t "Storage-Init" "Mermory"
+
+mkfs.ext4 -F /dev/mmcblk0p26 2>/dev/null
+mkfs.ext4 -F /dev/mmcblk0p25 2>/dev/null
+PART_2G=$(ls -1 /dev/mmcblk0p* 2>/dev/null | grep -E "p27|p28" | head -n 1)
+[ -n "$PART_2G" ] && mkfs.ext4 -F $PART_2G 2>/dev/null
+
+UUID_P26=$(block info /dev/mmcblk0p26 2>/dev/null | awk -F"UUID=" '{print $2}' | cut -d'"' -f2)
+UUID_P25=$(block info /dev/mmcblk0p25 2>/dev/null | awk -F"UUID=" '{print $2}' | cut -d'"' -f2)
+UUID_P2G=$(block info $PART_2G 2>/dev/null | awk -F"UUID=" '{print $2}' | cut -d'"' -f2)
+
+if [ -n "$UUID_P26" ] && [ -n "$UUID_P25" ]; then
+cat << FSTAB > /etc/config/fstab
 config global
-        option anon_swap '0'
-        option anon_mount '0'
-        option auto_swap '1'
-        option auto_mount '1'
-        option delay_root '5'
-        option check_fs '0'
+option anon_swap '0'
+option anon_mount '0'
+option auto_swap '1'
+option auto_mount '1'
+option delay_root '5'
+option check_fs '0'
 
 config mount
-        option target '/overlay'
-        option uuid '$UUID_P26'
-        option fstype 'ext4'
-        option options 'rw,sync'
-        option enabled '1'
+option target '/overlay'
+option uuid '$UUID_P26'
+option fstype 'ext4'
+option options 'rw,sync'
+option enabled '1'
 
 config mount
-        option target '/opt'
-        option uuid '$UUID_P25'
-        option fstype 'ext4'
-        option options 'rw,sync'
-        option enabled '1'
+option target '/opt'
+option uuid '$UUID_P25'
+option fstype 'ext4'
+option options 'rw,sync'
+option enabled '1'
 FSTAB
-        if [ -n "$UUID_P2G" ]; then
-            cat << FSTAB_2G >> /etc/config/fstab
+if [ -n "$UUID_P2G" ]; then
+cat << FSTAB_2G >> /etc/config/fstab
 
 config mount
-        option target '/emmc_2G'
-        option uuid '$UUID_P2G'
-        option fstype 'ext4'
-        option options 'rw,sync'
-        option enabled '1'
+option target '/emmc_2G'
+option uuid '$UUID_P2G'
+option fstype 'ext4'
+option options 'rw,sync'
+option enabled '1'
 FSTAB_2G
-        fi
-        
-        mkdir -p /mnt/p22_tmp
-        mount /dev/mmcblk0p22 /mnt/p22_tmp 2>/dev/null
-        if [ $? -eq 0 ]; then
-            mkdir -p /mnt/p22_tmp/upper/etc/config /mnt/p22_tmp/etc/config
-            cp -f /etc/config/fstab /mnt/p22_tmp/upper/etc/config/fstab 2>/dev/null
-            cp -f /etc/config/fstab /mnt/p22_tmp/etc/config/fstab 2>/dev/null
-            sync && umount /mnt/p22_tmp
-        fi
-        
-        touch /etc/config/storage_matrix_done
-        sync
-        logger -t "Stealth-Init" "Reboot"
-        reboot -f
-    fi
+fi
+
+mkdir -p /mnt/p22_tmp
+mount /dev/mmcblk0p22 /mnt/p22_tmp 2>/dev/null
+if [ $? -eq 0 ]; then
+mkdir -p /mnt/p22_tmp/upper/etc/config /mnt/p22_tmp/etc/config
+cp -f /etc/config/fstab /mnt/p22_tmp/upper/etc/config/fstab 2>/dev/null
+cp -f /etc/config/fstab /mnt/p22_tmp/etc/config/fstab 2>/dev/null
+sync && umount /mnt/p22_tmp
+fi
+
+touch /etc/config/storage_matrix_done
+sync
+logger -t "Stealth-Init" "Reboot"
+reboot -f
+fi
 fi
 exit 0
 EOF
 chmod +x package/base-files/files/etc/uci-defaults/99-firstboot-stealth-init
-echo "CONFIG_KERNEL_SKB_EXTENSIONS=y" >> .config
-echo "CONFIG_KERNEL_NET_TC_SKB_EXT=y" >> .config
-echo "CONFIG_KERNEL_NET_RX_BUSY_POLL=y" >> .config
 
-cat << 'EOF_KERNEL_MUTATIONS' >> .config
-# CONFIG_KERNEL_NF_CONNTRACK_DSCPREMARK_EXT is not set
-# CONFIG_KERNEL_NF_CONNTRACK_CHAIN_EVENTS is not set
-# CONFIG_KERNEL_NF_CONNTRACK_RTCACHE is not set
-# CONFIG_KERNEL_NF_CONNTRACK_MARK_EXT is not set
-# CONFIG_KERNEL_SKB_RECYCLE is not set
-# CONFIG_KERNEL_SKB_RECYCLE_MAX_PREALLOC is not set
-# CONFIG_KERNEL_OPENVSWITCH_NSS is not set
-EOF_KERNEL_MUTATIONS
+find target/linux/qualcommax -name "config-*" -o -name "config-default" | while read -r kernel_cfg; do
+    
+    echo "CONFIG_SKB_EXTENSIONS=y" >> "$kernel_cfg"
+    echo "CONFIG_NET_TC_SKB_EXT=y" >> "$kernel_cfg"
+    echo "CONFIG_NET_RX_BUSY_POLL=y" >> "$kernel_cfg"
+    
+    echo "# CONFIG_NF_CONNTRACK_DSCPREMARK_EXT is not set" >> "$kernel_cfg"
+    echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> "$kernel_cfg"
+    echo "# CONFIG_NF_CONNTRACK_RTCACHE is not set" >> "$kernel_cfg"
+    echo "# CONFIG_SKB_RECYCLE is not set" >> "$kernel_cfg"
+    echo "# CONFIG_SKB_RECYCLE_MAX_PREALLOC is not set" >> "$kernel_cfg"
+done
 
 logger -t "Kernel-Fix" "Fix finish!"
