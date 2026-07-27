bb499353da5030ad2603aac5d31e85a3599785b0
Zirui Shao <purewater@mail.ustc.edu.cn>
2026-07-27 22:40:57 +0800

    Update grep and sed commands in diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find . -type f -exec grep -lE "sbwml|0001-QUIC|temp_openwrt|package_libs_nghttp3" {} + | \
+find . -type f -not -path "*/\.git/*" -not -path "*/\.svn/*" -exec grep -lE "sbwml|0001-QUIC|temp_openwrt|package_libs_nghttp3" {} + | \
 xargs -r sed -i -E '/sbwml|0001-QUIC|temp_openwrt|package_libs_nghttp3/d'
