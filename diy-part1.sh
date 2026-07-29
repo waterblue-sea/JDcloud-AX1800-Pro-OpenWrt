d89edf0b5dd7fee56839e2cd52772cdac1699577
Zirui Shao <purewater@mail.ustc.edu.cn>
2026-07-30 04:39:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sed -i 's/;master/;openwrt-23.05/g' feeds.conf.default
-sed -i 's/;main/;openwrt-23.05/g' feeds.conf.default
-
-sed -i -E 's/\.git$/.git;openwrt-23.05/g' feeds.conf.default
+sed -i -E 's/^(src-git (packages|luci|routing|telephony).*)\.git[;a-zA-Z0-9_-]*$/\1.git;openwrt-23.05/g' feeds.conf.default
 
 mkdir -p scripts
 cat << 'EOF' > scripts/prepare_dependencies.sh
